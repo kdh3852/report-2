ba2da49ba49ee0a39a70e8ed26de955ab0cdecf7
kdh3852 <62225351+kdh3852@users.noreply.github.com>
2020-04-05 01:04:05 +0900

    Add files via upload

---
+++ src/sele.java
@@ -0,0 +1,12 @@
+
+public class sele {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sale[] = new int [] {157, 209, 251, 312};
+		
+		for(int i=0; i<4; i++)
+			System.out.printf("%d/4ºÐ±â : sale[%d]=%d%n", i+1, i, sale[1]);
+	}
+
+}
