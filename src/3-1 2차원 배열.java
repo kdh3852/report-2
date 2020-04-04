2f0a5275231ef2c314eafd7e13ffd769ab86a9ef
kdh3852 <62225351+kdh3852@users.noreply.github.com>
2020-04-05 01:01:51 +0900

    Rename src/sale2.java to  src/3-1 2차원 배열.java

--- src/3-1 2차원 배열.java
+++ src/3-1 2차원 배열.java
@@ -7,7 +7,7 @@ public class sale2 {
 		
 		for(int i = 0; i<2; i++) {
 			for(int j = 0; j<4; j++)
-					System.out.printf("%d/4�б� : sale [%d][%d]=%d%n",j+1,i,j,sale[i][j]);
+					System.out.printf("%d/4ºÐ±â : sale [%d][%d]=%d%n",j+1,i,j,sale[i][j]);
 			System.out.println();
 				
 		}
