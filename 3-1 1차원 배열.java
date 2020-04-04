1f7b1409044e49007a9485ef6194d0ac935c81e2
kdh3852 <62225351+kdh3852@users.noreply.github.com>
2020-04-05 01:02:54 +0900

    Rename src/sele.java to 3-1 1차원 배열.java

--- 3-1 1차원 배열.java
+++ 3-1 1차원 배열.java
@@ -6,7 +6,7 @@ public class sele {
 		int sale[] = new int [] {157, 209, 251, 312};
 		
 		for(int i=0; i<4; i++)
-			System.out.printf("%d/4�б� : sale[%d]=%d%n", i+1, i, sale[1]);
+			System.out.printf("%d/4ºÐ±â : sale[%d]=%d%n", i+1, i, sale[1]);
 	}
 
 }
